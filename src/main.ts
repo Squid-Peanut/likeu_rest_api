d257b7d02509874e1287207bebe1c60f17aa7dee
snilsnil <heo6601@naver.com>
2024-05-31 21:55:02 +0900

    add CORS

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ import * as path from 'path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true, // true 시 모든 url에 개방(개발 환경). 특정 url만 허용할 수 있음(배포 환경)
+    credentials: true, // 프론트에서 credentials 설정을 true 해주었기 때문
+  });
+
   app.use(
     '/users',
     express.static(path.join(__dirname, '..', 'src', 'users', 'uploads')),
